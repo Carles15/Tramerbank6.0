36d49a323bce9ca306d4262f03acd1dab38a8fd2
Carles15 <Magiic15@gmail.com>
2015-02-16 13:25:09 +0100

    Merge origin/Juan into Juan
    Conflicts:
    	src/java/com/fpmislata/banco/presentacion/controller/dominio/TransaccionController.java
    	src/java/com/fpmislata/banco/servicios/BalanceadorDeDatos.java

--- web/pasarela/successfullPayment/successfullPayment.js
+++ web/pasarela/successfullPayment/successfullPayment.js
@@ -1,4 +1,37 @@
-app.controller("SuccessfullPaymentController", ["$scope", "$http", "$rootScope","$window", function($scope, $http, $rootScope, $window) {
+app.controller("SuccessfullPaymentController", ["$scope", "$http", "$rootScope", "$window", function ($scope, $http, $rootScope, $window) {
         $('header').hide();
-}]);
+        $scope.onClickButtonDescargar = function () {
+            alto = 30;
+            ancho = -10;
+
+            var doc = new jsPDF();
+
+            doc.setFontSize(30);
+            doc.text(ancho + 30, 20, 'Factura de pago');
+
+            doc.setFontSize(16);
+            doc.text(ancho + 30.5, 27, 'Datos emitidos por Tramerbank, "El primer banco Jedi".');
+            doc.setFontSize(14);
+
+
+            doc.text((ancho + 30), (alto + 20), 'Cuenta');
+            doc.text((ancho + 55), (alto + 20), 'IdCompra');
+            doc.text((ancho + 90), (alto + 20), 'Importe');
+            doc.text((ancho + 120), (alto + 20), 'Concepto');
+
+            doc.setFontSize(12);
+            doc.text((ancho + 30), (alto + 32), $rootScope.peticionPago.codigoCuentaClienteOrigen.toString());
+            doc.text((ancho + 55), (alto + 32), $rootScope.peticionPago.idCarrito);
+            doc.text((ancho + 90), (alto + 32), $rootScope.peticionPago.importe);
+            doc.text((ancho + 120), (alto + 32), $rootScope.peticionPago.concepto);
+
+
+            doc.line(ancho + 28, alto + 25, ancho + 150, alto + 25);
+            doc.line(ancho + 52, alto + 15, ancho + 52, alto + 50);
+            doc.line(ancho + 88, alto + 15, ancho + 88, alto + 50);
+            doc.line(ancho + 118, alto + 15, ancho + 118, alto + 50);
+
+            doc.save('datauri');
+        };
+    }]);
 
